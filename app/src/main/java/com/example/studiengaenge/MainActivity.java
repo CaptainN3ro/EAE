ab50217889ee42c2560d70f843d99363b8ee0515
nisc0055 <nisc0055@stud.hs-kl.de>
2020-08-05 13:24:34 +0200

    Checkboxes

--- app/src/main/java/com/example/studiengaenge/MainActivity.java
+++ app/src/main/java/com/example/studiengaenge/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.studiengaenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TableRow;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,8 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinnerDienste;
-    ListView ListViewDienste;
-    Button button;
+    LinearLayout my_layout;
 
     String[] streamingdienste;
     List<String> Amazon;
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         setTitle(" NWT - Never Watch Twice ");
         spinnerDienste = findViewById(R.id.STREAMINGDIENSTE);
-        ListViewDienste = findViewById(R.id.SERIEN);
+        my_layout = findViewById(R.id.SERIEN);
 
 
         fillData();
@@ -76,26 +79,71 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         if(i==0){
-            ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, alleSerien);
-            ListViewDienste.setAdapter(listViewAdapter);
+
+            int Array_Count = alleSerien.size();
+
+            for (int z=0; z< Array_Count; z++){
+                CheckBox cb = new CheckBox(this);
+                cb.setText(alleSerien.get(z));
+                cb.setId(z+6);
+                my_layout.addView(cb);
+
+            }
+            //ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, alleSerien);
+            //ListViewDienste.setAdapter(listViewAdapter);
         }
         if(i==1){
-            ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Netflix);
-            ListViewDienste.setAdapter(listViewAdapter);
-        }
+
+            my_layout.removeAllViews();
+            int Array_Count = Amazon.size();
+
+            for (int z=0; z< Array_Count; z++) {
+                CheckBox cb = new CheckBox(this);
+                cb.setText(Amazon.get(z));
+                cb.setId(z + 6);
+                my_layout.addView(cb);
+                //ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Netflix);
+                //ListViewDienste.setAdapter(listViewAdapter);
+            }
+            }
         if(i==2){
-            ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Amazon);
-            ListViewDienste.setAdapter(listViewAdapter);
+            my_layout.removeAllViews();
+            int Array_Count = Netflix.size();
+
+            for (int z=0; z< Array_Count; z++){
+                CheckBox cb = new CheckBox(this);
+                cb.setText(Netflix.get(z));
+                cb.setId(z+6);
+                my_layout.addView(cb);
+        }
+            //ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Amazon);
+            //ListViewDienste.setAdapter(listViewAdapter);
         }
         if(i==3){
-            ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Disney);
-            ListViewDienste.setAdapter(listViewAdapter);
+            my_layout.removeAllViews();
+            int Array_Count = Disney.size();
+
+            for (int z=0; z< Array_Count; z++){
+                CheckBox cb = new CheckBox(this);
+                cb.setText(Disney.get(z));
+                cb.setId(z+6);
+                my_layout.addView(cb);
+        }
+            //ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Disney);
+            //ListViewDienste.setAdapter(listViewAdapter);
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-        ArrayAdapter listViewAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, alleSerien);
-        ListViewDienste.setAdapter(listViewAdapter);
+        int Array_Count = alleSerien.size();
+
+        for (int z=0; z< Array_Count; z++){
+            CheckBox cb = new CheckBox(this);
+            cb.setText(alleSerien.get(z));
+            cb.setId(z+6);
+            my_layout.addView(cb);
+
+        }
     }
 }
