f5d1726ed50a29bdb815be8ebf19e6c9de7ff4e4
rotr0003 <rotr0003@stud.hs-kl.de>
2020-08-20 16:59:54 +0200

    Added Bouncy-Button

--- app/src/main/java/com/example/nwt/PreActivity.java
+++ app/src/main/java/com/example/nwt/PreActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,6 +29,13 @@ public class PreActivity extends AppCompatActivity {
 
         start = findViewById(R.id.START_APP);
 
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(start, PropertyValuesHolder.ofFloat("scaleX", 1.2f), PropertyValuesHolder.ofFloat("scaleY", 1.2f), PropertyValuesHolder.ofFloat("alpha", 1f));
+        animator.setDuration(1000);
+
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.setRepeatMode(ObjectAnimator.REVERSE);
+
+        animator.start();
         start.setOnClickListener(this::startApp);
     }
 
